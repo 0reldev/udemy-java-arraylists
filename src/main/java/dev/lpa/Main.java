2e91a78707251c827407797156b46fb4b64b9603
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-06-19 16:46:23 +0200

    Lesson part 2 introducing the set, remove, of and addAll methods.

--- src/main/java/dev/lpa/Main.java
+++ src/main/java/dev/lpa/Main.java
@@ -7,6 +7,11 @@ record GroceryItem(String name, String type, int count) {
     public GroceryItem(String name) {
         this(name, "DAIRY", 1);
     };
+
+    @Override
+    public String toString() {
+        return String.format("%d %s in %s", count, name.toUpperCase(), type);
+    }
 }
 
 public class Main {
@@ -24,6 +29,11 @@ public class Main {
         objectList.add("Yogurt");
 
         ArrayList<GroceryItem> groceryList = new ArrayList<>();
-        groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("butter"));
+        groceryList.add(new GroceryItem("milk"));
+        groceryList.add(new GroceryItem("oranges", "PRODUCE", 5));
+        groceryList.set(0, new GroceryItem("apples", "PRODUCE", 6));
+        groceryList.remove(1);
+        System.out.println(groceryList);
     }
 }
