d3eb3e8715cce5ee3bb5c45a0d3d2611e684ff0b
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-06-19 17:13:16 +0200

    Lesson part 3
    Introducing the contains, indexOf, lastIndexOf, remove, removeAll, addAll and sort methods.

--- src/main/java/dev/lpa/MoreLists.java
+++ src/main/java/dev/lpa/MoreLists.java
@@ -1,6 +1,8 @@
 package dev.lpa;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class MoreLists {
@@ -20,5 +22,43 @@ public class MoreLists {
 
         groceries.addAll(nextList);
         System.out.println(groceries);
+
+        System.out.println("Third item = " + groceries.get(2));
+        if(groceries.contains("mustard")) {
+            System.out.println("List contains mustard");
+        }
+
+        groceries.add("yogurt");
+        System.out.println("first = " + groceries.indexOf("yogurt"));
+        System.out.println("last = " + groceries.lastIndexOf("yogurt"));
+
+        System.out.println(groceries);
+        groceries.remove(1);
+        System.out.println(groceries);
+        groceries.remove("yogurt");
+        System.out.println(groceries);
+
+        groceries.removeAll(List.of("apples", "eggs"));
+        System.out.println(groceries);
+
+        groceries.retainAll(List.of("apples", "milk", "mustard", "cheese"));
+        System.out.println(groceries);
+
+        groceries.clear();
+        System.out.println(groceries);
+        System.out.println("isEmpty = " + groceries.isEmpty());
+
+        groceries.addAll(List.of("apples", "milk", "mustard", "cheese"));
+        groceries.addAll(Arrays.asList("eggs", "pickles", "mustard", "ham"));
+
+        System.out.println(groceries);
+        groceries.sort(Comparator.naturalOrder());
+        System.out.println(groceries);
+
+        groceries.sort(Comparator.reverseOrder());
+        System.out.println(groceries);
+
+        var groceryArray = groceries.toArray(new String[groceries.size()]);
+        System.out.println(Arrays.toString(groceryArray));
     }
 }
